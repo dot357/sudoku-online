2d32ec280ac6b52955bd6a03a84f82312631989e
Emre Can Eskimez <dot357@users.noreply.github.com>
2025-12-27 00:10:05 +0100

    Feature/dockerize (#8)
    * docker and docker compose ready
    
    * vercel build command added

--- server/db/prisma.ts
+++ server/db/prisma.ts
@@ -1,6 +1,8 @@
 
 import { PrismaBetterSqlite3 } from "@prisma/adapter-better-sqlite3"
-import { PrismaClient } from "@prisma/client"
+import prismaPkg from "@prisma/client"
+
+const { PrismaClient } = prismaPkg
 
 const globalForPrisma = globalThis as unknown as { prisma?: PrismaClient }
 
